ddd4288580b6d8381fdccdaa01b63b992a91900c
Slipmaks <slipmaks@gmail.com>
2023-01-10 14:55:14 +0200

    counter add

--- components/TheProductInfo.vue
+++ components/TheProductInfo.vue
@@ -2,7 +2,7 @@
   <div class="product-header d-flex align-items-center justify-center">
     <h1 style="font-weight: 700">
       Омега-3
-      <i style="font-weight: 400; font-family: ''Piazzolla' serif">Тріска</i>
+      <i style="font-weight: 400; font-family: 'Piazzolla' serif">Тріска</i>
     </h1>
     <span class="badge rounded-pill bg-light text-dark px-5 py-2 ms-4"
       >з вітамінами А і D3</span
@@ -165,31 +165,147 @@
         </p>
       </div>
     </div>
+    <div class="product__to-cart d-flex flex-column">
+      <div class="d-flex flex-row justify-content-between align-items-center">
+        <div
+          class="product__to-cart-in-package d-flex align-items-center gap-5"
+        >
+          <img src="~/assets/foil 1.svg" alt="" />
+          <div>
+            <h2>В упаковці:</h2>
+            <p>120 капсул по 500мг</p>
+            <p>1 місяць прийому</p>
+          </div>
+        </div>
+        <div class="product__to-cart-price d-flex gap-4 align-items-center">
+          <h2>963 ₴</h2>
+          <h2>{{ currentPrice }} ₴</h2>
+        </div>
+      </div>
+      <div class="d-flex flex-row justify-content-between">
+        <div class="product__to-cart-count d-flex align-items-center">
+          <button @click="decreaseCount" :disabled="count == 1">-</button>
+          <input type="text" v-model="count" />
+          <button @click="increaseCount">+</button>
+        </div>
+        <div class="product__to-cart-add">
+          <button>До кошика</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const count = ref(1);
+const currentPrice = computed(() => {
+  return count.value * 489;
+});
+const increaseCount = () => {
+  count.value = count.value + 1;
+};
+const decreaseCount = () => {
+  count.value = count.value - 1;
+};
+</script>
 <style scoped lang="scss">
+@import "~/assets/css/variables.scss";
 .product {
   &-header {
-    color: #00284f;
+    color: $main-blue;
   }
   &__tab {
     &-buttons {
-      border-bottom: 1px solid rgba(0, 40, 79, 0.3);
+      border-bottom: 1px solid $light-blue;
     }
     &-button {
       font-size: 17px;
-
+      color: $light-blue;
+      transition: color 0.6s;
       &:hover {
-        border-top: 1px solid rgba(0, 40, 79, 0.3);
+        border-top: 1px solid $light-blue;
+        color: $main-blue;
       }
       &.active {
-        border-top: 1px solid rgba(0, 40, 79, 0.3);
+        color: $main-blue;
+        border-top: 1px solid $light-blue;
       }
     }
   }
   &-overview {
     margin-top: 32px;
-    color: #00284f;
+    color: $main-blue;
+  }
+  &__to-cart {
+    margin-top: 56px;
+    &-in-package {
+      & img {
+        width: 40px;
+        height: 40px;
+      }
+      font-size: 18px;
+      line-height: 23px;
+      & div p,
+      h2 {
+        margin: 0;
+      }
+      & div p:last-child {
+        color: $light-blue;
+      }
+    }
+    &-add {
+      & button {
+        border: none;
+        outline: none;
+        background: rgba(105, 197, 34, 1);
+        color: #ffffff;
+        border-radius: 40px;
+      }
+    }
+    &-count {
+      & button {
+        border: none;
+        outline: none;
+        background: none;
+        font-size: 40px;
+        line-height: 7px;
+        color: $light-blue;
+        &:hover {
+          color: $main-blue;
+        }
+      }
+      & input {
+        text-align: center;
+        width: 46px;
+        height: 46px;
+        border: none;
+        outline: none;
+        border-radius: 7px;
+      }
+    }
+    &-price {
+      & h2 {
+        &:first-child {
+          position: relative;
+          color: $light-blue;
+          font-weight: 900;
+          font-size: 28px;
+          line-height: 36px;
+          &:before {
+            border-bottom: 3px solid $main-blue;
+            position: absolute;
+            content: "";
+            width: 100%;
+            height: 50%;
+            transform: rotate(15deg);
+          }
+        }
+        &:last-child {
+          font-weight: 700;
+          font-size: 40px;
+          line-height: 51px;
+        }
+      }
+    }
   }
 }
 .tab-pane {
